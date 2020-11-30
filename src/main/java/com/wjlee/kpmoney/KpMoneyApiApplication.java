0793c368fc46c9e6f9f02fc75e418c221273a0bf
bestwjin <65703752+bestwjin@users.noreply.github.com>
2020-11-30 13:26:49 +0900

    Update KpMoneyApiApplication.java

--- src/main/java/com/wjlee/kpmoney/KpMoneyApiApplication.java
+++ src/main/java/com/wjlee/kpmoney/KpMoneyApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class KpMoneyApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KpMoneyApiApplication.class, args);
 	}
